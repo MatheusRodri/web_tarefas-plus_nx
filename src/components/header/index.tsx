d56248e05a6a08e5a29ecc4657a618c512d887c7
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-17 11:05:10 -0300

    add header and initial style

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import styles from './styles.module.css';
+
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <section className={styles.headerContent}>
+                <nav className={styles.headerNav}>
+                    <Link href="/">
+                        <h1 className={styles.logo}>
+                            Tarefas<span>+</span>
+                        </h1>
+                    </Link>
+
+                    <Link href="/dashboard" className={styles.dashboardLink}>
+                        Meu Painel
+                    </Link>
+                </nav>
+
+                <button className={styles.loginButton}>Acessar</button>
+
+
+            </section>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/home.module.css";
+
+import heroImg from "../../public/assets/hero.png"
 
 
 
@@ -12,7 +13,33 @@ export default function Home() {
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
 
-      <h1>Teste</h1>
+      <main className={styles.main}>
+
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo do Tarefas+"
+            src={heroImg}
+            priority
+          />
+        </div>
+
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br/> suas tarefas e estudos
+        </h1>
+
+        
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+
+          <section className={styles.box}>
+            <span>+90 comentário</span>
+          </section>
+
+        </div>
+      </main>
     </div>
   );
 }
