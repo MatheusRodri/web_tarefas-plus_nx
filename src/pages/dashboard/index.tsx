09a4578f95eb06379e567ed283ff8730c31a8586
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-23 21:21:21 -0300

    add a private route

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from './styles.module.css'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
 
 export default function Dashboard() {
     return (
@@ -11,4 +13,21 @@ export default function Dashboard() {
             <h1 className={styles.title}>Bem-vindo ao seu painel!</h1>
         </div>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const session = await getSession({ req })
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }  
+    return {
+        props:{},
+    }
 }
