f7a2c7f9108da993b0c9f3f7db35d1008133bc85
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-08-15 11:52:42 -0300

    CRITICAL UPDATE

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDN9WpxmePFE1fzwyoAPdbJnMlRfW0MaAs",
-  authDomain: "tarefas-plus-cf0a9.firebaseapp.com",
-  projectId: "tarefas-plus-cf0a9",
-  storageBucket: "tarefas-plus-cf0a9.firebasestorage.app",
-  messagingSenderId: "484094969928",
-  appId: "1:484094969928:web:8ccc2d4968c9d7d740307d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
